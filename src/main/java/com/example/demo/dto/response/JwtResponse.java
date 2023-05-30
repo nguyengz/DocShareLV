7a98389b5247adc56ee0a7a09f15f54dfdc0bcf0
java <htvdongthap@gmail.com>
2023-05-30 11:04:10 +0700

    first

--- src/main/java/com/example/demo/dto/response/JwtResponse.java
+++ src/main/java/com/example/demo/dto/response/JwtResponse.java
@@ -29,6 +29,14 @@ public class JwtResponse {
         this.id=id;
     }
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getToken() {
         return token;
     }
